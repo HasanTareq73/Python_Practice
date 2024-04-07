717c2aa656e1f072241d164aebd98f8863d9ed71
hasantareq73 <Mehedi Hasan Tareq>
2024-04-07 21:24:00 +0600

    First Commit

---
+++ Hello_world.py
@@ -0,0 +1,20 @@
+print ("Hello world")
+
+# this is a comment 
+
+"""
+Hello, this is Mahdi, I'm learner 
+
+"""
+if 5 > 2 :
+    print("Five is greater then two")
+
+X=5
+Y= 'Mahdi'
+Z= 3.1416
+
+print(f"this is {X} ")
+print (f"this is {Y}")
+
+
+print(Z)
